0a47314a98498b2a35a2a4e508d5957afd080a11
root <ngomfa@gmail.com>
2018-11-16 15:39:44 +0100

    exo2

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>PHP Partie1_Exo2</title>
+</head>
+<body>
+  <?php
+  $lastname = "DOE";
+  $firstname ="John";
+  $age = 17;
+    echo $lastname . " "; // Pour faire la concaténation dans PHP on utilise un . 
+    echo $firstname . " ";
+    echo $age;
+  ?>
+</body>
+</html>
